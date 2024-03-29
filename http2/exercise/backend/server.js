8d13bbb0e6fc2d68745ad64bf5cc1e393cacc519
Kevin Y. Kim <kevinyckim33@gmail.com>
2024-03-28 20:51:46 -0500

    http2 frontend understood a bit better

--- http2/exercise/backend/server.js
+++ http2/exercise/backend/server.js
@@ -1,7 +1,9 @@
-import http2 from "http2";
+import http2 from "http2"; // not using express; getting lower level for fundamentals
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+
+// from vercel
 import handler from "serve-handler";
 import nanobuffer from "nanobuffer";
 
@@ -23,6 +25,8 @@ msg.push({
 // openssl x509 -req -days 365 -in csr.pem -signkey key.pem -out server.crt
 //
 // http2 only works over HTTPS
+// ^ and that's what all that hoopla are for...
+// need certs for HTTP2...okay...
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const server = http2.createSecureServer({
   cert: fs.readFileSync(path.join(__dirname, "/../server.crt")),
@@ -35,6 +39,36 @@ const server = http2.createSecureServer({
  *
  */
 
+// writing on fly
+server.on("stream", (stream, headers) => {
+  const path = headers[":path"];
+  const method = headers[":method"];
+
+  // streams open for every request from the browser
+  // this handles the initial case of user opening the app/page
+  // and the FE calling getNewMsgs()
+  if (path === "/msgs" && method === "GET") {
+    console.log("only once!");
+    // immediately reply with 200 OK and the encoding
+    console.log("connected a stream! " + stream.id);
+    stream.respond({
+      ":status": 200,
+      "content-type": "text/plain; charset=utf-8", // jsons don't stream very well; yaml does (though it sucks)
+    });
+
+    // congrats user, you connected to our stream!
+    // here's all our messages right now in the msg array
+    stream.write(JSON.stringify({ msg: getMsgs() }));
+    connections.push(stream);
+
+    stream.on("close", () => {
+      console.log("disconnected " + stream.id);
+      connections = connections.filter((s) => s !== stream);
+    });
+  }
+});
+
+// prewritten
 server.on("request", async (req, res) => {
   const path = req.headers[":path"];
   const method = req.headers[":method"];
@@ -44,7 +78,11 @@ server.on("request", async (req, res) => {
     return handler(req, res, {
       public: "./frontend",
     });
-  } else if (method === "POST") {
+  }
+  // handling when any user posts a message
+  else if (method === "POST") {
+    console.log("posting!");
+    // essentially bodyparser
     // get data out of post
     const buffers = [];
     for await (const chunk of req) {
@@ -53,11 +91,22 @@ server.on("request", async (req, res) => {
     const data = Buffer.concat(buffers).toString();
     const { user, text } = JSON.parse(data);
 
-    /*
-     *
-     * some code goes here
-     *
-     */
+    msg.push({
+      user,
+      text,
+      time: Date.now(),
+    });
+
+    res.end(); // why res.end() here??
+    // all requests must end
+
+    // hello every stream in the connections
+    // all the streams now display all the messages with the newly msg just added in
+    connections.forEach((stream) => {
+      // could be written smarter than just dumping all messages in the array
+      // but not the point of the exercise
+      stream.write(JSON.stringify({ msg: getMsgs() }));
+    });
   }
 });
 
--- http2/exercise/frontend/http2-chat.js
+++ http2/exercise/frontend/http2-chat.js
@@ -1,6 +1,6 @@
 const chat = document.getElementById("chat");
 const msgs = document.getElementById("msgs");
-const presence = document.getElementById("presence-indicator");
+const presence = document.getElementById("presence-indicator"); // the red/green dot
 
 // this will hold all the most recent messages
 let allChat = [];
@@ -11,6 +11,7 @@ chat.addEventListener("submit", function (e) {
   chat.elements.text.value = "";
 });
 
+// unchanged from polling
 async function postNewMsg(user, text) {
   const data = {
     user,
@@ -32,11 +33,54 @@ async function postNewMsg(user, text) {
 }
 
 async function getNewMsgs() {
-  /*
-   *
-   * code goes here
-   *
-   */
+  console.log("trigger getNewMsgs");
+  let reader;
+  const utf8Decoder = new TextDecoder("utf-8");
+  try {
+    console.log("about to fetch /msgs");
+    const res = await fetch("/msgs");
+    // can't use res.json here
+    // since by design, our reader will never finish
+    // our reader is a STREAM!
+    reader = res.body.getReader(); // turns to readable text stream
+  } catch (e) {
+    console.log("connection error", e);
+  }
+  // presence.innerText = "🟢";
+  presence.innerText = "huh";
+
+  let readerResponse;
+  let done;
+  do {
+    try {
+      console.log("do try go forever?");
+      // stuck here forever until there's another
+      // stream.write call from backend
+      // the initial GET calls a stream.write
+      // subsequest POSTs call stream.write
+      readerResponse = await reader.read();
+      console.log("readerResponse is: ", readerResponse);
+    } catch (e) {
+      console.error("reader fail", e);
+      presence.innerText = "🔴";
+      return;
+    }
+    console.log("do i hit here repeatedely?");
+    const chunk = utf8Decoder.decode(readerResponse.value, { stream: true });
+    done = readerResponse.done;
+
+    if (chunk) {
+      try {
+        const json = JSON.parse(chunk);
+        allChat = json.msg;
+        console.log("do i render repeatedely?");
+        render();
+      } catch (e) {
+        console.error("parse error", e);
+      }
+    }
+  } while (!done);
+  presence.innerText = "🔴";
 }
 
 function render() {
