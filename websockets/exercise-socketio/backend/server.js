c4aa1fd9737093fc5426e444e1379c13b6812ac9
Kevin Y. Kim <kevinyckim33@gmail.com>
2024-03-29 22:53:12 -0500

    realtime fem complete

--- websockets/exercise-socketio/backend/server.js
+++ websockets/exercise-socketio/backend/server.js
@@ -1,7 +1,8 @@
 import http from "http";
 import handler from "serve-handler";
 import nanobuffer from "nanobuffer";
-import { Server } from "socket.io";
+import { Server } from "socket.io"; // this one comes from node_modules
+// which now I'm wondering...why aren't we doing this for frontend??
 
 const msg = new nanobuffer(50);
 const getMsgs = () => Array.from(msg).reverse();
@@ -19,11 +20,43 @@ const server = http.createServer((request, response) => {
   });
 });
 
-/*
- *
- * Code goes here
- *
- */
+// code starts here
+// #1. Hey I want to upgrade this server to accept websocket connection
+const io = new Server(server, {});
+
+// subtle awesomeness
+// if you disconnect the server, and reconnect
+// the connection gets automatically restored
+// vs the websocket by hand method or vs polling backoff and retry
+// built in out of the box;
+// backoff/retry logic not fun, but out of the box with socket.io
+io.on("connection", (socket) => {
+  console.log(`connected: ${socket.id}`);
+  // i.e. connected: yoDMoJHe_2X9RZA5AAAB
+
+  // go send to FE these getMsgs() on their msg:get listener
+  socket.emit("msg:get", { msg: getMsgs() }); // arbitrarily named
+
+  socket.on("disconnect", () => {
+    console.log(`disconnected: ${socket.id}`);
+  });
+
+  // msg:post must be in sync with FE
+  socket.on("msg:post", (data) => {
+    msg.push({
+      user: data.user,
+      text: data.text,
+      time: Date.now(),
+    });
+    // rebroadcast
+    // before: websocket by hand
+    // loop through channels and run this function for each socket
+    // now: io represents the server holding all the sockets
+    io.emit("msg:get", { msg: getMsgs() });
+  });
+});
+
+// code ends here
 
 const port = process.env.PORT || 8080;
 server.listen(port, () =>
--- websockets/exercise-socketio/frontend/socketio-chat.js
+++ websockets/exercise-socketio/frontend/socketio-chat.js
@@ -1,15 +1,40 @@
-// a global called "io" is being loaded separately
-
 const chat = document.getElementById("chat");
 const msgs = document.getElementById("msgs");
 const presence = document.getElementById("presence-indicator");
 let allChat = [];
 
-/*
- *
- * Code goes here
- *
- */
+// socket.io will downgrade itself to polling if websocket ability not available
+// for fun; let's just set WebSocket to null;
+// window.WebSocket = null;
+// hindsight...I don't think this does anything
+// always seem to poll initially before upgrading connection
+// now the requests in network tab downgrade to polling!
+// crazy!!
+// http://localhost:8080/socket.io/?EIO=4&transport=polling&t=OwCuLmZ
+// a global called "io" is being loaded separately
+// from a cdn script tag
+
+// #1. Hey I want to upgrade this to a Websocket connection
+const socket = io("http://localhost:8080"); // io we get globally
+
+// out of the box, retry/backoff logic already done for you
+socket.on("connect", () => {
+  console.log("connected"); // yup, BE has websockets, and now so do you!
+  presence.innerText = "🟢";
+});
+
+socket.on("disconnect", () => {
+  presence.innerText = "🔴";
+});
+
+// must be consistently named msg:get with BE; but can be named everything;
+// just in sync
+socket.on("msg:get", (data) => {
+  allChat = data.msg;
+  render();
+});
+
+// code ends here
 
 chat.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -18,11 +43,13 @@ chat.addEventListener("submit", function (e) {
 });
 
 async function postNewMsg(user, text) {
-  /*
-   *
-   * Code goes here
-   *
-   */
+  // new code
+  const data = { user, text };
+
+  // .emit and .on go both ways from FE to BE and BE to FE
+  // .emit: send to other end this
+  // .on: when other end sends something through this, get the data and do whatever cb to it
+  socket.emit("msg:post", data);
 }
 
 function render() {
